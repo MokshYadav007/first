4315a771d5af5b0794e412509a96501e6f064cbd
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-12 11:31:17 +0530

    lambdaExpression.java

--- lambdaExpression.java
+++ lambdaExpression.java
@@ -2,13 +2,16 @@ interface addition{
     int add(int a, int b);
 }
 
-public class newJavaFile3
+public class lambdaExpression
 {
     public static void main(String[] args) {
+        
 
+        //without return keyword
         addition ad1= (a,b)-> (a+b);
         System.out.println(ad1.add(20,32));
-
+        
+        //with return keyword
         addition ad2= (a,b)-> {
             return (a+b);
         };
