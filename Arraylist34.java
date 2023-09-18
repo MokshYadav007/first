22e46a44adfe4792c9aa9b6aceef44f77d9fb16a
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-18 19:16:46 +0530

    Create Arraylist34.java

---
+++ Arraylist34.java
@@ -0,0 +1,24 @@
+package collection;
+import java.util.*;
+
+public class Arraylist34
+{
+    public static void main(String[] args) {
+        
+        ArrayList ar= new ArrayList();
+        ar.add(100);
+        ar.add(200);
+        ar.add(560);
+        System.out.println(ar);
+
+        ArrayList ar2 = new ArrayList();
+        ar2.add("string");
+        ar2.add(56);
+        ar2.add('t');
+        ar2.add(56.6);
+      System.out.println(ar2);
+
+      ar2.add(3, "*********");
+      System.out.println(ar2);
+    }
+}
