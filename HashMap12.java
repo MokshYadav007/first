c4799b5bb21b9d58f55e93d5e3af7f7e8b16f1c1
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-30 17:34:57 +0530

    Create HashMap12.java

---
+++ HashMap12.java
@@ -0,0 +1,28 @@
+package collection;
+import java.util.*;
+
+public class HashMap12 {
+    public static void main(String[] args) {
+
+          HashMap hm = new HashMap();
+          hm.put(01, "this");
+          hm.put(02, "is");
+          hm.put(03, "hashmap");
+
+          System.out.println(hm);
+
+          HashMap hm1 = new HashMap();
+          hm1.put("yes", "this");
+          hm1.put("new", "is");
+          hm1.put("hashtable", "hashmap");
+
+          System.out.println(hm1);
+
+          LinkedHashMap hm2 = new LinkedHashMap();
+          hm2.put("yes", "this");
+          hm2.put("new", "is");
+          hm2.put("hashtable", "hashmap");
+
+          System.out.println(hm2);
+    }
+}
